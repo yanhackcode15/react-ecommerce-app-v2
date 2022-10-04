b105e0a98c33c60e3d45216036e97d13c88de88b
yanhackcode15 <iamyanhu@gmail.com>
2022-10-04 15:41:49 -0700

    minor text update

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     const {cartedItems} =React.useContext(Context)
     return (
         <header>
-            <Link exact="true" to="/"><h2>Shop</h2></Link>
+            <Link exact="true" to="/"><h2>React Online Store</h2></Link>
             <Link to="/cart"><i className={`ri-shopping-cart-${cartedItems.length?"fill":"line"} ri-fw ri-2x`}></i></Link>
         </header>
     )
